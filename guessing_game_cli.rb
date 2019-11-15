976dc59ffa1eb3ee708489412e2b8f5591737f61
Melike KILIC <melike180400@gmail.com>
2019-11-15 18:34:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ puts "Type a number between 1 and 6"
 input = gets.chomp
 if input==rand
   puts "You guessed the correct number!"
-elsif input = !rand
+elsif input != rand
   puts "Sorry! The computer guessed <number>."
 else input== "exit"
   puts "Goodbye!"
